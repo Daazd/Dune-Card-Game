9d7d46f13e912360268d07a3f502962fb14851d7
Daazd <zoomie067@yahoo.com>
2024-07-25 13:53:44 -0700

    fixing deployment backend

--- Dunecardgame/Dunecardgame/settings.py
+++ Dunecardgame/Dunecardgame/settings.py
@@ -113,14 +113,15 @@ WSGI_APPLICATION = 'Dunecardgame.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': os.environ.get('DB_NAME', 'dune_card_game'),
+        'USER': os.environ.get('DB_USER', 'taylor'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'Emerica23#'),
+        'HOST': os.environ.get('DB_HOST', 'postgres-service'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
