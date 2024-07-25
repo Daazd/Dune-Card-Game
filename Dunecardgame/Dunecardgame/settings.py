966b1e30f13c05a980616f4c9c8adb22031ad182
Daazd <zoomie067@yahoo.com>
2024-07-24 20:57:50 -0700

    pivoting to linode

--- Dunecardgame/Dunecardgame/settings.py
+++ Dunecardgame/Dunecardgame/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'gameapp.apps.GameappConfig',
+    'gameapp',
     'rest_framework.authtoken',
     'rest_framework_simplejwt.token_blacklist',
     'rest_framework',
